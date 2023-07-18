c1a1faeeb693f3543db4e4fc3fc5e7d27cfd30e7
Merve UĞURSAÇ <merve.ugursac34@gmail.com>
2023-07-18 08:08:08 +0300

    Add : Create ASP.NET project to dockerize

---
+++ Docker_Dockerize.AspNetCoreMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Docker_Dockerize.AspNetCoreMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
